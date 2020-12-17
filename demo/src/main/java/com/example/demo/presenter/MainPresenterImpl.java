a2d5feaa47a6420020708e7bb5c93cfa89530712
j1953179110 <1953179110@qq.com>
2020-12-17 20:56:20 +0800

    第十六次提交

---
+++ demo/src/main/java/com/example/demo/presenter/MainPresenterImpl.java
@@ -0,0 +1,4 @@
+package com.example.demo.presenter;
+
+public class MainPresenterImpl {
+}
--- demo/src/main/java/com/example/demo/utils/RetrofitUtils.java
+++ demo/src/main/java/com/example/demo/utils/RetrofitUtils.java
@@ -2,13 +2,24 @@ package com.example.demo.utils;
 
 import com.example.demo.api.ApiService;
 import com.example.demo.api.URLContract;
+import com.example.demo.interfaces.ICallBack;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.net.URL;
 
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
-public class RetrofitUtils {
+public class RetrofitUtils implements INetWorkInterface {
 
     public static RetrofitUtils instance;
 
@@ -36,4 +47,43 @@ public class RetrofitUtils {
     }
 
 
+    @Override
+    public <T> void get(String url, ICallBack<T> iCallBack) {
+        apiService
+                .getnewlist(url)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            Type[] genericInterfaces = iCallBack.getClass().getGenericInterfaces();
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type t = actualTypeArguments[0];
+                            Gson gson = new Gson();
+                            T result = gson.fromJson(string, t);
+                            iCallBack.onsuccess(result);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
 }
